a75aab67508f576d94117c571ada6616970dbd7d
ItzStevee <117988858+ItzSteveefr@users.noreply.github.com>
2026-05-07 11:18:13 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,23 @@
 import "./globals.css";
 import ClientLayout from "@/client-layout";
 import { ViewTransitions } from "next-view-transitions";
+import Script from "next/script";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="c3cef3db-76be-410d-8982-612717e4f09b"
+          strategy="afterInteractive"
+        />
+      </body>
+    </html>
+  );
+}
 
 export const metadata = {
   title: "UnderFive Studios",
